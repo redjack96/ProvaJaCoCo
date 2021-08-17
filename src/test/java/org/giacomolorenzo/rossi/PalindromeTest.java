758f73c8b555a889f02fa9c966504964ef14b830
Giacomo <giacomo.redjack@gmail.com>
2021-08-17 16:01:32 +0200

    Aggiunto un profilo per testing e coverage on-the-fly. Il codice del progetto è disponibile.

--- src/test/java/org/giacomolorenzo/rossi/PalindromeTest.java
+++ src/test/java/org/giacomolorenzo/rossi/PalindromeTest.java
@@ -4,6 +4,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * Per provare la coverage on-the-fly e redigere il report, usa il comando
+ * mvn -P coverageProfile clean package
+ *
+ * Se vuoi la coverage on-the-fly, ma non il report:
+ * mvn -P coverageProfile clean test
+ *
+ * Per fare il testing senza coverage, usa il comando
+ * mvn test
+ */
 public class PalindromeTest {
     @Test
     public void whenEmptyString_thenAccept() {
